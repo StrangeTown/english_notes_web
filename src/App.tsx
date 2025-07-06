40748af0cb5e96d1cb4c7aa85eb6fa1a46db8f0d
Xing <yinxingdyx@163.com>
2025-07-06 23:27:55 +0800

    Fix React Router basename for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import Home from './pages/Home';
 function App() {
 
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/english_notes_web">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/peppa-pig/:episode" element={<PeppaPig />} />
